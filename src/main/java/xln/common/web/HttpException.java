cd5e727889884450388e00cdfa561fe850f32ad9
rex.lien <line@TW-LW10219-M-2.local>
2018-11-02 16:38:48 +0800

    Update

--- src/main/java/xln/common/web/HttpException.java
+++ src/main/java/xln/common/web/HttpException.java
@@ -2,9 +2,11 @@ package xln.common.web;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.http.HttpStatus;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 public class HttpException extends RuntimeException {
 
