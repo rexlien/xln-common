7935b6bbf288a07932fd15c66e59d0bc45506c44
rex.lien <rexlien@LINE_TW_a0b286.local>
2019-06-28 17:31:11 +0800

    Update swagger2 when webmvc setup
    According to springboot document, both webmvc and web flux can be linked together, but will end up as mvc by default using tomcat, which still support reactive. enable_webflux is still needed to start as a netty server(pure starter-webflux).

--- src/main/java/xln/common/web/config/Swagger2WebmvcConfig.java
+++ src/main/java/xln/common/web/config/Swagger2WebmvcConfig.java
@@ -1,23 +1,22 @@
 package xln.common.web.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.reactive.config.ResourceHandlerRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@ConditionalOnWebApplication(type=ConditionalOnWebApplication.Type.REACTIVE)
+
 @Configuration
-//@EnableSwagger2WebFlux
-@Profile({"xln-swagger2-webflux"})
-public class Swagger2WebfluxConfig implements WebFluxConfigurer {
-
+@EnableSwagger2
+@Profile({"xln-swagger2-webmvc"})
+public class Swagger2WebmvcConfig  {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -27,12 +26,16 @@ public class Swagger2WebfluxConfig implements WebFluxConfigurer {
                 .build();
     }
 
+/*
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
         //swagger resource
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
         //registry.addResourceHandler("/swagger/**").addResourceLocations("classpath:/static/swagger/");
-        registry.addResourceHandler("/swagger/**").addResourceLocations("classpath:/static/swagger/");
+        //registry.addResourceHandler("/swagger/**").addResourceLocations("classpath:/static/swagger/");
     }
+
+*/
 }
