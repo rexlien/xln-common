9a3079370c62a4737e279f196d5832b7280af906
rex-lien <line@TW-LW10219-M-2.local>
2019-10-17 13:34:41 +0800

    Update

--- src/main/java/xln/common/annotation/EnvironmentCondition.java
+++ src/main/java/xln/common/annotation/EnvironmentCondition.java
@@ -1,14 +1,23 @@
 package xln.common.annotation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.core.type.classreading.AnnotationMetadataReadingVisitor;
+import org.springframework.stereotype.Component;
+import xln.common.Context;
 
+@Component
 public class EnvironmentCondition implements Condition {
 
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+
+        String enable = System.getenv("XLN_ENVCOND_ENABLE");
+        if(enable == null || !enable.equals("true")) {
+            return true;
+        }
 
         if(metadata instanceof AnnotationMetadataReadingVisitor) {
 
