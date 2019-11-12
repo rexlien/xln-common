ddef920023cf4f4966fb44dab088bb9b9985f0eb
rex-lien <line@TW-LW10219-M-2.local>
2019-11-12 17:29:34 +0800

    Update

--- src/main/java/xln/common/config/R2DBCConfig.java
+++ src/main/java/xln/common/config/R2DBCConfig.java
@@ -4,25 +4,35 @@ import com.github.jasync.r2dbc.mysql.JasyncConnectionFactory;
 import com.github.jasync.sql.db.mysql.pool.MySQLConnectionFactory;
 import com.github.jasync.sql.db.mysql.util.URLParser;
 import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableR2dbcRepositories
+@EnableTransactionManagement
 public class R2DBCConfig extends AbstractR2dbcConfiguration {
 
     @Value("${xln.r2dbc-config.url:mysql://localhost:3306}")
     private String url;
 
     @Override
-    //@Bean
+    @Bean
     public ConnectionFactory connectionFactory() {
 
         return new JasyncConnectionFactory(new MySQLConnectionFactory(
                 URLParser.INSTANCE.parseOrDie(url,  URLParser.INSTANCE.getDEFAULT().getCharset())
         ));
+    }
+
+    @Bean
+    public ReactiveTransactionManager reactiveTransactionManager(@Autowired ConnectionFactory connectionFactory) {
+        return new R2dbcTransactionManager(connectionFactory);
     }
 }
