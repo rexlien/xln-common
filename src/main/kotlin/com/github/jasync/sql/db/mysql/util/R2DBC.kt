46e4b4a703b5414dd56312d128699c6c33220863
rex-lien <line@TW-LW10219-M-2.local>
2020-03-10 18:16:24 +0800

    Update

---
+++ src/main/kotlin/com/github/jasync/sql/db/mysql/util/R2DBC.kt
@@ -0,0 +1,33 @@
+package com.github.jasync.sql.db.mysql.util
+
+import com.github.jasync.sql.db.Configuration
+import com.github.jasync.sql.db.SSLConfiguration
+import com.github.jasync.sql.db.util.AbstractURIParser
+import java.nio.charset.Charset
+import java.time.Duration
+
+object R2DBCURLParser : AbstractURIParser() {
+    override val DEFAULT = Configuration(
+            username = "root",
+            host = "127.0.0.1", //Matched JDBC default
+            port = 3306,
+            password = null,
+            database = null
+    )
+
+    override val SCHEME = "^mysql$".toRegex()
+
+    override fun assembleConfiguration(properties: Map<String, String>, charset: Charset): Configuration {
+        return DEFAULT.copy(
+                username = properties.getOrElse(USERNAME) { DEFAULT.username },
+                password = properties[PASSWORD],
+                database = properties[DBNAME],
+                host = properties.getOrElse(HOST) { DEFAULT.host },
+                port = properties[PORT]?.toInt() ?: DEFAULT.port,
+                ssl = SSLConfiguration(properties),
+                charset = charset,
+                queryTimeout = Duration.ofSeconds(5)
+        )
+    }
+
+}
