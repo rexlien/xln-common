37365c72e4a90b8eb5e56b43a5c44ea5ed816013
rex-lien <line@TW-LW10219-M-2.local>
2020-06-04 19:05:46 +0800

    Update

--- src/main/java/xln/common/utils/HttpUtils.java
+++ src/main/java/xln/common/utils/HttpUtils.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -34,15 +36,19 @@ public class HttpUtils {
             if (urlValues == null) {
 
                 return methodCall.uri(url).headers((t) -> {
-                    for (Map.Entry<String, String> e : headers.entrySet()) {
-                        t.add(e.getKey(), e.getValue());
+                    if(headers != null) {
+                        for (Map.Entry<String, String> e : headers.entrySet()) {
+                            t.add(e.getKey(), e.getValue());
+                        }
                     }
                 }).retrieve().bodyToMono(type);
 
             } else {
                 return methodCall.uri(url, urlValues).headers((t) -> {
-                    for (Map.Entry<String, String> e : headers.entrySet()) {
-                        t.add(e.getKey(), e.getValue());
+                    if(headers != null) {
+                        for (Map.Entry<String, String> e : headers.entrySet()) {
+                            t.add(e.getKey(), e.getValue());
+                        }
                     }
                 }).retrieve().bodyToMono(type);
             }
@@ -59,8 +65,14 @@ public class HttpUtils {
         return httpCallMono(url, urlValues, method, type, headers, gson.toJson(body));
     }
 
-    public static <T> Mono<T> httpCallMono(String url, Map<String, ?> urlValues, HttpMethod method, Class type,
-            Map<String, String> headers, String body) {
+    public static <T> Mono<ResponseEntity<T>> httpCallMonoResponseEntity(String url, Map<String, ?> urlValues, HttpMethod method, Class type,
+                                                                    Map<String, String> headers, Object body) {
+        Gson gson = new Gson();
+        return httpCallMono(url, urlValues, method, headers,gson.toJson(body)).toEntity(type);
+    }
+
+    public static WebClient.ResponseSpec httpCallMono(String url, Map<String, ?> urlValues, HttpMethod method,
+                                                      Map<String, String> headers, String body) {
         try {
             log.debug("Call http url:" + url);
             var methodCall = reactiveClient.method(method);
@@ -72,8 +84,10 @@ public class HttpUtils {
             }
 
             requestSpec = requestSpec.headers((t) -> {
-                for (Map.Entry<String, String> e : headers.entrySet()) {
-                    t.add(e.getKey(), e.getValue());
+                if(headers != null) {
+                    for (Map.Entry<String, String> e : headers.entrySet()) {
+                        t.add(e.getKey(), e.getValue());
+                    }
                 }
                 if (body != null && !body.isEmpty()) {
                     t.add("Content-Type", "application/json");
@@ -83,12 +97,20 @@ public class HttpUtils {
             if (body != null && !body.isEmpty()) {
                 requestSpec.body(BodyInserters.fromValue(body));
             }
-            return requestSpec.retrieve().bodyToMono(type);
+            return requestSpec.retrieve();
 
         } catch (Exception ex) {
             log.error("", ex);
-            return Mono.empty();
+            return null;
         }
+    }
+
+
+
+    public static <T> Mono<T> httpCallMono(String url, Map<String, ?> urlValues, HttpMethod method, Class type,
+            Map<String, String> headers, String body) {
+
+        return httpCallMono(url, urlValues, method, headers, body).bodyToMono(type);
     }
 
     public static <T> Flux<T> httpGetFlux(String url, Class type) {
